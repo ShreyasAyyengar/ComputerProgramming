ab320cc180d39dff2f64f0de01e14d993794e218
Shreyas A <shreyas.ayyengar@gmail.com>
2020-11-12 12:12:55 +0800

    Notes for main.py!

---
+++ me/shreyasayyengar/computerprogramming/main.py
@@ -0,0 +1,4 @@
+# The main class is used as the main coding file for the lesson.
+# Once complete / satisfactory, it should be moved to a new package
+# with with lesson name, and a new filed with the lesson name and description
+# check file structure for more information.
