acbb78f5ab08c7d424829ad5bf44fc72a6f00c4f
Shreyas A <shreyas.ayyengar@gmail.com>
2020-11-18 10:35:49 +0800

    New day, new code!

--- me/shreyasayyengar/computerprogramming/Lesson 7/Lesson 7.1 (Lists).py
+++ me/shreyasayyengar/computerprogramming/Lesson 7/Lesson 7.1 (Lists).py
@@ -1,15 +1,45 @@
 foods = ["Apple Slices", "Pizza", "Pasta", "Steak", "Bread", "Chocolate Bar"]
 prices = [50, 150, 200, 250, 120, 80]
+healthy = ["Healthy", "UnHealthy", "Healthy", "Healthy", "Healthy", "UnHealthy"]
 counter = 0
 
 while counter < len(foods):
-    print(f"{counter + 1} {foods[counter]}: {prices[counter]}")
+    print(f'{counter + 1} {foods[counter]} {prices[counter]}')
     counter += 1
 
+done = len(foods) + 1
+print(f'{done}  ----- Choose this number to finish your order.')
 
-while 1 == 1:
-    foodchoice = int(input("\n\nWhat would you like to eat today? Please either give the food choice. \n\n"))
-    print(f'You chose {foods[foodchoice - 1]}. It will amount to {prices[foodchoice - 1]} PHP')
+orders = [0] * len(foods)
+choice = int(input('What food item would you like?\n\n'))
+print(f'{done}  ----- Choose this number to finish your order.')
 
-done = len()
-print(f'{done} ----- Choose this number to complete your order!')
+orders = [0] * len(foods)
+choice = int(input('What food item would you like?\n\n'))
+
+while choice != done:
+    while choice <= 0 or choice > len(foods) + 1:
+        print("Sorry, you have not entered a valid number. Please only choose valid food items. ")
+        choice = int(input('What food item would you like?\n\n'))
+
+    orders[choice - 1] += 1
+    choice = int(input('What additional food item would you like?\n\n'))
+
+total = 0
+healthpoints = 0
+for x, y, z, h in zip(foods, orders, prices, healthy):
+    total += y * z
+    if y > 0:
+        print(f'Excellent choice. You chose {y} x {x}.')
+    if h == 'Healthy':
+        healthpoints += y
+    else:
+        healthpoints -= y
+
+if healthpoints < 0:
+    print(f'You must reorder. You need to have more health points! Your healthpoints is {healthpoints}')
+    choice = int(input('What food item would you like?\n\n'))
+
+else:
+    print(f'Good job! You ate healthy')
+print(f' Your grand total is {total}. Bon appetit!')
--- me/shreyasayyengar/computerprogramming/main.py
+++ me/shreyasayyengar/computerprogramming/main.py
@@ -4,3 +4,43 @@
 # check file structure for more information.
 
 # -------------------------------------
+foods = ["Apple Slices", "Pizza", "Pasta", "Steak", "Bread", "Chocolate Bar"]
+prices = [50, 150, 200, 250, 120, 80]
+counter = 0
+
+while counter < len(foods):
+    print(f'{counter + 1} {foods[counter]} {prices[counter]}')
+    counter += 1
+
+done = len(foods) + 1
+print(f'{done}  ----- Choose this number to finish your order.')
+
+orders = [0] * len(foods)
+choice = int(input('What food item would you like?\n\n'))
+print(f'{done}  ----- Choose this number to finish your order.')
+
+orders = [0] * len(foods)
+choice = int(input('What food item would you like?\n\n'))
+
+while choice != done:
+    try:
+        a = len(foods)
+        print
+        a[choice]
+    except IndexError:
+        print("You chose an invalid number!")
+
+    # while choice <= 0 or choice > len(foods) + 1:
+    #     print("Sorry, you have not entered a valid number. Please only choose valid food items. ")
+    #     choice = int(input('What food item would you like?\n\n'))
+
+    orders[choice - 1] += 1
+    choice = int(input('What additional food item would you like?\n\n'))
+
+total = 0
+for x, y, z in zip(foods, orders, prices):
+    total += y * z
+    if y > 0:
+        print(f' Excellent choice. You chose {y} x {x}.')
+
+print(f' Your grand total is {total}. Bon appetit!')
